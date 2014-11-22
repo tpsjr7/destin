a4151b43b8bfbc199d85bba13334670cf6f825bd
Ted Sanders <tpsjr7@gmail.com>
2014-11-22 13:56:37 -0600

    created generate_atoms.sh

---
+++ Destin/Common/generate_atoms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p trees
+for layers in 1 2 3 4 5 6 7 ; do
+
+    for trees in 1 3 10 ; do
+        ./openCogAtomGenerator $layers $trees > trees/L${layers}_T${trees}.scm
+    done
+done
